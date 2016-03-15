543f63d89dba503ed35a1276eb9823b03c57bc8d
mohammadfarooqi <mohammad.farooqi@gmail.com>
2016-03-15 16:10:32 -0400

    initial commit

---
+++ CMFGlobalFundingRates_v2/Scripts/MyApp.js
@@ -0,0 +1,8 @@
+﻿(function () {
+    var app = angular.module('myApp', []);
+
+    app.controller('HomeController', ['$scope', function ($scope) {
+        $scope.message = 'This';
+        $scope.panelView = 'AngularPartials/_panelView.html';
+    }]);
+})();
